c96f07447907740a8561193b12ab2a3bd20bbc8f
Son <Sonntuet1997@gmail.com>
2018-05-01 20:18:41 +0700

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private elRef: ElementRef) {
+
+  }
+
+  ngOnInit(): void {
+    const loadingElement = document.getElementById('Loading');
+    loadingElement.classList.add('hide-loading');
+    setTimeout(x => {
+      loadingElement.parentNode.removeChild(loadingElement);
+    }, 1400);
+  }
 }
--- src/share/not-found/not-found.component.ts
+++ src/share/not-found/not-found.component.ts
@@ -5,25 +5,6 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent implements OnInit{
-  title = 'app';
-  height: number = document.documentElement.clientHeight;
-  width: number = document.documentElement.clientWidth;
-  @ViewChild('notFoundContent') notFoundContent: ElementRef;
-  heightContent: number;
-
-  onResize() {
-    this.height = document.documentElement.clientHeight;
-    this.width = document.documentElement.clientWidth;
-    this.heightContent = this.height / 2 - this.notFoundContent.nativeElement.clientHeight / 2;
-  }
-
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-    this.heightContent = this.height / 2 - this.notFoundContent.nativeElement.clientHeight / 2;
-  }
+export class NotFoundComponent {
 
 }
